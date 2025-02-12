ccb7346fb6b11ac75a0ba0369334e08a35a4cff2
rahul-b-p <rahulzantoz10@gmail.com>
2025-02-12 16:00:55 +0530

    feat:Add new env variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,10 +2,19 @@ import { config } from "dotenv";
 import { ERROR } from "../constants";
 
 config();
-const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error(ERROR.MONGODB_URI_REQUIRED);
-}
+const requiredEnvVariables: string[] = [
+    'MONGODB_URI',
+    'DB_NAME'
+];
 
-export default MONGODB_URI as string;
+// Throwing application error, to exist from process when env variables are not assigned
+requiredEnvVariables.forEach((envVar) => {
+    if (!process.env[envVar]) {
+        throw new Error(ERROR.ENV_REQUIRED);
+    }
+});
+
+
+export const MONGODB_URI = process.env.MONGODB_URI as string;
+export const DB_NAME = process.env.DB_NAME as string;
